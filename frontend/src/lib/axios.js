0fc8a2f45f3975a0ed8ca0dee5a979622c1ca61d
Manav Gohil <gohilmanav2005@gmail.com>
2025-06-06 16:23:29 +0530

    commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    // baseURL: import.meta.env.VITE_API_URL,
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://chat-app-ten-coral-50.vercel.app/api ",
+    // baseURL: "http://localhost:5001/api",
     withCredentials: true
 })
 
